3bd6020cb6a9b30bfc0fac8b633ab4553099f8f6
Hoang An <antnh@icd-vn.com>
2020-04-24 15:15:49 +0700

    cm

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+
+// Routes
+import IndexRoutes from './routes/index.routes';
+import PostRoutes from './routes/post.routes';
+
+export class App {
+    private app: Application;
+
+    constructor(private port?: number | string) {
+        this.app = express();
+        this.settings();
+        this.middlewares();
+        this.routes();
+    }
+
+    settings() {
+        this.app.set('port', this.port || process.env.PORT || 5001);
+    }
+
+    middlewares() {
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(IndexRoutes);
+        this.app.use('/posts', PostRoutes);
+    }
+
+    async listen() {
+        await this.app.listen(this.app.get('port'));
+        console.log(`Server running on port ${this.app.get('port')}`);
+    }
+}
---
+++ src/routes/post.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+const router = Router();
+
+import { getPosts, createPost, getPost, deletePost, updatePost } from '../controllers/post.controller';
+
+router.route('/')
+    .get(getPosts)
+    .post(createPost);
+
+router.route('/:postId')
+    .get(getPost)
+    .delete(deletePost)
+    .put(updatePost);
+
+export default router;
